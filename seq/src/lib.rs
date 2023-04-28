85697ff509fad6c12a249cb5f4e07ba0e8d16882
Lana Scravaglieri <lana.scravaglieri@ifpen.fr>
2023-04-28 15:49:22 +0200

    Tests 1 and 2 of seq proc macro

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,8 +1,40 @@
-use proc_macro::TokenStream;
+use proc_macro::{Literal, TokenStream, TokenTree};
+use quote::{format_ident, quote};
+use syn::{parse::Parse, parse::ParseStream, parse::Result, parse_macro_input, Expr, Token};
+
+struct Sequence {
+    i: String,
+    start: u32,
+    stop: u32,
+    handle: TokenStream,
+}
+
+impl Parse for Sequence {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let i = input.parse::<syn::Ident>()?;
+        let i = i.to_string();
+        input.parse::<Token![in]>()?;
+        let start = input.parse::<syn::LitInt>()?;
+        let start = start.base10_parse::<u32>()?;
+        input.parse::<Token![..]>()?;
+        let stop = input.parse::<syn::LitInt>()?;
+        let stop = stop.base10_parse::<u32>()?;
+        let handle = input.parse::<Expr>()?;
+        let handle = TokenStream::from(quote! {#handle});
+        Ok(Sequence {
+            i,
+            start,
+            stop,
+            handle,
+        })
+    }
+}
 
 #[proc_macro]
 pub fn seq(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input = parse_macro_input!(input as Sequence);
 
-    unimplemented!()
+    let expanded = replace(input.i, 1, input.handle);
+
+    TokenStream::from(expanded)
 }
